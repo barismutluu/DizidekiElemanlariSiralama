82b88ff232a96c91ef32d2f6880261fd3d4cda16
Barış Mutlu <baris_mutlu@outlook.com.tr>
2024-03-01 02:53:10 +0300

    Yorum satırı eklendi.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        // Kullanıcıdan değer alınması için Scanner tanımlanıyor
+        Scanner input = new Scanner(System.in);
+        //Kullanıcıdan dizi boyutunun değeri alınıyor
         System.out.print("Dizinin boyutu n :");
-        int diziBoyutu=input.nextInt();
+        int diziBoyutu = input.nextInt();
+        //Dizi tanımı yapılıyor
         int[] numbers = new int[diziBoyutu];
-
-        for (int i=0;i<diziBoyutu;i++){
-            System.out.print(i+1 +". Elemanı :");
-            numbers[i]= input.nextInt();
+        //Kullanıcından aldığımız dizi boyutu kadar eleman tanımlanması isteniyor
+        for (int i = 0; i < diziBoyutu; i++) {
+            System.out.print(i + 1 + ". Elemanı :");
+            numbers[i] = input.nextInt();
         }
-
+        // Kullanıcıdan alınan verilerle oluşturulan dizi küçükten büyüğe sıralanıyor ve ekrana yazdırılıyor
         Arrays.sort(numbers);
-        System.out.println("Sıralama :"+Arrays.toString(numbers));
+        System.out.println("Sıralama :" + Arrays.toString(numbers));
     }
 }
